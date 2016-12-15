e3c6623bf68804ab1c63140dbd8a38e686789df8
mahdiASC <mahdi@allstarcode.org>
2016-12-15 17:45:57 -0500

    almost done

--- main.R
+++ main.R
@@ -159,12 +159,12 @@ cleanData<-keepData[,-c(14,15,23)]
 catData<-cleanData[,c(3:6,8:15,17:25,28)]
 numData<-cleanData[,c(2,26:28,30:32,36)]
 
-rm(cleanData,keepData, testDF)
-gc()
+#rm(cleanData,keepData, testDF)
+#gc()
 
-#################
+########################
 ##EXPLORATORY ANALYSIS##
-#################
+########################
 
 #HOW TO HANDLE DIFFERENT DATA
 #Dependent Variable   Categorical   Continuous
@@ -198,7 +198,7 @@ data.frame(sort(myNAs,decreasing=TRUE))
 #If too much of a difference will revisit
 
 #websiteScore has too much missing data
-numData<-numData[,-6]
+numData<-numData[,-c(5:7)]
 
 mosthighlycorrelated <- function(mydataframe,numtoreport)
 {
@@ -237,11 +237,196 @@ pcaNoRead.NAs<-prcomp(na.omit(NUMnoReadScore), retx=TRUE, center=TRUE, scale=TRU
 #PCA with/imputed NAs
 library(mice)
 #Responds much better to additive effects on Variance in the data (First two components account for over 80% of the variance in the data)
-pcaNoApp.imputed<-prcomp(complete(mice(NUMnoAppScoreData, printFlag = FALSE)))#More std. dev. than without NAs
-pcaNoRead.imputed<-prcomp(complete(mice(NUMnoReadScore, printFlag = FALSE)))#More std. dev. than without NAs 
+pcaNoApp.imputed<-prcomp(complete(mice(NUMnoAppScoreData, printFlag = FALSE)),retx=TRUE, center=TRUE, scale=TRUE)#More std. dev. than without NAs
+pcaNoRead.imputed<-prcomp(complete(mice(NUMnoReadScore, printFlag = FALSE)),retx=TRUE, center=TRUE, scale=TRUE)#More std. dev. than without NAs 
 #http://stats.stackexchange.com/questions/72839/how-to-use-r-prcomp-results-for-prediction
 
 #Best accounting for variation in data w/Cumulative Proportion
 summary(pcaNoApp.imputed)
 
+#all suggest all 3 components are significant (>10% of variation each)
+#I'll use all 3 and imputed without App (best first component)
+#Checking for important variables for first components
+pcaNoApp.imputed$rotation[,1]
+#Qscore and GPA are important in 1st component
+pcaNoApp.imputed$rotation[,2]
+#GPA and Qscore are most important in 2nd component
+pcaNoApp.imputed$rotation[,3]
+#GPA and readscore are most important in 3rd component
 
+###########################
+##MAKING PREDICTIVE MODEL##
+###########################
+almostNumData<-complete(mice(NUMnoAppScoreData, printFlag = FALSE))
+almostNumData$zerotoeight<-numData$zerotoeight
+
+library(caret)
+library(MASS)
+finalNumData<-preProcess(almostNumData, c("BoxCox", "center", "scale"))
+finalNumData<-data.frame(predict(finalNumData, almostNumData), stringsAsFactors = FALSE)
+
+myLDA<-lda(zerotoeight~., data = finalNumData)
+myPrediction<-predict(myLDA)
+
+table(finalNumData$zerotoeight, myPrediction$class)
+#Low sensitivity
+#high selectivity (just labels almost everyone as unchecked!)
+
+#Acc
+#62/80 = 77.5%
+
+#High QScore
+by(finalNumData$Qscore, finalNumData$zerotoeight, FUN=mean)
+
+#Not high ReadScore
+by(finalNumData$ReadScore, finalNumData$zerotoeight, FUN=mean)
+
+#not high GPA
+by(finalNumData$scaledGPA, finalNumData$zerotoeight, FUN=mean)
+
+#           Qscore  ReadScore  scaledGPA
+#checked   4.421053  4.368421 0.8615789
+#unchecked 4.229508  4.934426 0.8768852
+
+############
+##Plotting##
+############
+library(ggplot2)
+prop.pca = pcaNoApp.imputed$sdev^2/sum(pcaNoApp.imputed$sdev^2)
+prop.lda = myLDA$svd^2/sum(myLDA$svd^2)
+
+dataset = data.frame(cats = finalNumData$zerotoeight, pca = pcaNoApp.imputed$x, lda = myPrediction$x)
+p1 <- ggplot(dataset) + geom_point(aes(LD1, c(0), size=LD1, colour = cats, shape="circle")) + facet_grid(cats ~ .)
+p2 <- ggplot(dataset) + geom_point(aes(pca.PC3, pca.PC4, size=pca.PC1, colour = cats, shape = "circle"))
+
+print(p2) #PCA - Good at reducing variables to most necessary
+print(p1) #LDA - Good at finding seperatability between groups
+
+####################
+##CATEGORICAL DATA##
+####################
+
+#chisq.test(table(dependent, independent))
+
+#further filtering cat.data
+alphaCatData<-catData[,-c(1,3,4,6,7)]
+
+#Q1
+correctQ1<-function(input){
+  if(input=="1" || input == "Figure A"){
+    return ("1")
+  }else{
+    return ("0")
+  }
+}
+
+alphaCatData$Q1<-sapply(alphaCatData$Q1, correctQ1)
+
+#Q2
+correctQ2<-function(input){
+  if(input=="1" || input == "forward(1), turnLeft(90),  forward(1),  turnLeft(90),  forward(1),  turnLeft(90),  forward(1)"){
+    return ("1")
+  }else{
+    return ("0")
+  }
+}
+
+alphaCatData$Q2<-sapply(alphaCatData$Q2, correctQ2)
+
+#Q3
+correctQ3<-function(input){
+  if(input=="1" || input == "\"Splinter\" is to \"Wood\""){
+    return ("1")
+  }else{
+    return ("0")
+  }
+}
+
+alphaCatData$Q3<-sapply(alphaCatData$Q3, correctQ3)
+
+#Q4
+correctQ4<-function(input){
+  if(input==1 || input == 89754){
+    return ("1")
+  }else{
+    return ("0")
+  }
+}
+
+alphaCatData$Q4<-sapply(alphaCatData$Q4, correctQ4)
+
+#Q5
+correctQ5<-function(input){
+  if(input=="1" || input == "Shape B"){
+    return ("1")
+  }else{
+    return ("0")
+  }
+}
+
+alphaCatData$Q5<-sapply(alphaCatData$Q5, correctQ5)
+
+#Exploring Data
+#https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0ahUKEwiQvs3w0O_QAhXqjlQKHYkJDEgQFggaMAA&url=http%3A%2F%2Fwww.ats.ucla.edu%2Fstat%2Fr%2Fdae%2Flogit.htm&usg=AFQjCNGT6-Qp31SYHb0DDSSHysaZHbRfpA&sig2=111mVOQ5TR7eT1jFfVg39g&bvm=bv.141320020,d.eWE
+
+
+#FIRST: Find independence/correlation with Chi Square test
+#chisq.test(table(alphaCatData$zerotoeight, alphaCatData$takenCSAPCourse))
+getChis<- function(sig = .1){
+  outcome <- list()
+  for (i in colnames(alphaCatData)[-17]){
+    x<-chisq.test(table(alphaCatData$zerotoeight, alphaCatData[[i]]))$p.value
+    if (x<sig){
+      outcome<-c(outcome, i)
+    }
+  }
+  return (outcome)
+}
+
+#At 10% significance, students having some CS experience from other courses outside of school or belonging to schools offering CS have a high liklihood of being a 0->8 student
+getChis()
+
+alphaCatData<-as.data.frame(apply(alphaCatData, 2, factor))
+#Can use glm to create predictive model for student (data must be dataframe with factor data)
+myGLM<-glm(zerotoeight~otherCourses+CSatSchool, data=alphaCatData, family = "binomial")
+
+myPred<-predict(myGLM, type = "response") #give predictions of what
+predMax<-as.numeric(names(table(myPred)[4]))
+
+convertCat <- function(x){
+  if (x>=predMax){
+    return ("checked")
+  }else{
+    return ("unchecked")
+  }
+}
+
+test<-sapply(myPred, convertCat)
+table(alphaCatData$zerotoeight, test)
+#Acc
+#46/80 = 57.5%
+
+#Making Confidence intervals
+#http://www.stat.columbia.edu/~martin/W2024/R11.pdf
+exp(confint.default(myGLM))
+
+##################################################
+##Anny's attempt at Combining Cat. and Num. data##
+##################################################
+
+#READ THROUGH THIS FIRST
+#https://www.r-bloggers.com/logistic-regression-and-categorical-covariates/
+
+#Clean up data (cleanData)
+
+cleanData$Q1<-sapply(cleanData$Q1, correctQ1)
+cleanData$Q2<-sapply(cleanData$Q2, correctQ2)
+cleanData$Q3<-sapply(cleanData$Q3, correctQ3)
+cleanData$Q4<-sapply(cleanData$Q4, correctQ4)
+cleanData$Q5<-sapply(cleanData$Q5, correctQ5)
+
+#Imputting only numbers
+cleanImput<-complete(mice(cleanData, printFlag = FALSE))
+test<-cleanImput[,-c(1,3:10,16,29,30,32:35)]
+
+x<-glm(zerotoeight~ReadScore+Qscore+otherCourses+CSatSchool, data= test, family = binomial(link = "logit"))
